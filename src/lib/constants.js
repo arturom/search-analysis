8271ad400753bfb31df6e15ecfe745b32e9aaacd
Arturo Mejia <arturomejiac@gmail.com>
2023-02-06 09:45:54 -0500

    Added more default params. Added future param configs

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,3 +1,39 @@
-export const builtInAnalyzers = ['standard', 'simple', 'whitespace', 'pattern', 'fingerprint', 'english', 'keyword'];
-export const builtInTokenizers = ['standard', 'letter', 'lowercase', 'whitespace', 'uax_url_email', 'classic', 'thai'];
-export const builtInFilters = ['lowercase', 'asciifolding', 'word_delimiter_graph'];
+export const builtInAnalyzers = [
+  // useless comment just to appease Prettier and let me add each value in a new line
+  'standard',
+  'simple',
+  'whitespace',
+  'pattern',
+  'fingerprint',
+  'english',
+  'keyword',
+];
+
+export const builtInTokenizers = [
+  'classic',
+  'edge_ngram',
+  'keyword',
+  'letter',
+  'lowercase',
+  'ngram',
+  'path_hieararchy',
+  'pattern',
+  'standard',
+  'thai',
+  'uax_url_email',
+  'whitespace',
+];
+
+export const builtInFilters = [
+  'apostrophe',
+  'asciifolding',
+  'edge_ngram',
+  'elision',
+  'fingerprint',
+  'keyword_repeat',
+  'kstem',
+  'limit',
+  'lowercase',
+  'ngram',
+  'word_delimiter_graph',
+];
